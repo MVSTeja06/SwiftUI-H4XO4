9beee50060c4c5727c0ccfc038d677c5ff44505c
amatcha <amatc001@odu.edu>
2021-10-20 18:14:36 -0400

    Initial Commit

---
+++ H4XOR News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR News
+//
+//  Created by Ajay Gupta on 10/20/21.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
